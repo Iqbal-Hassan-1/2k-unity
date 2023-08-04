72dd61d8c9fbc1067a159a11a4e2c2650afadceb
iqbal hassan <emiqball111@gmail.com>
2023-08-04 10:25:35 +0500

    updated rank page

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -14,10 +14,10 @@ const Ranking = () => {
         <Col md={{ offset: 1, span: 2 }} className={style.forPlayer}>
           <img src={player} alt="profile" />
         </Col>
-        <Col md={{ offset: 3, md: 6 }} className="pt-1 ">
+        <Col md={{ offset: 3, span: 6 }} className="pt-1 ">
           <div className="d-flex align-items-center gap-3">
             <h4 className="fw-bold">Daily Gamer</h4>
-            <button className="px-4 py-1 bg-primary text-white border-0">
+            <button className="px-4 py-2 bg-primary text-white border-0 fw-bold user">
               Message User
             </button>
           </div>
@@ -29,12 +29,12 @@ const Ranking = () => {
             </button>
           </div>
           <Row className="px-1 pt-1">
-            <Col md={2}>
+            <Col md={4}>
               <p className="mb-0 text-secondary">ArcheType</p>
               <p className="mb-0 text-secondary">Height</p>
               <p className="mb-0 text-secondary">Position</p>
             </Col>
-            <Col md={2}>
+            <Col md={4}>
               {" "}
               <p className="mb-0">Athletic Finisher</p>
               <p className="mb-0">6.6</p>
@@ -42,32 +42,85 @@ const Ranking = () => {
             </Col>
           </Row>
           <Row className="d-flex gap-2 px-1 pt-1">
-            <Col md={2}>
-              <div className=" text-center">
-                <p className="bg-secondary p-2 mb-0">88%</p>
-                <span style={{ fontSize: "0.9rem" }}>Rec win percentage</span>
+            <Col md={3}>
+              <div className={`text-center ${style.forRec}`}>
+                <p className="p-2 mb-0">88%</p>
+                <span style={{ fontSize: "0.9rem" }} className="text-secondary">
+                  Rec win percentage
+                </span>
               </div>
             </Col>{" "}
-            <Col md={2}>
-              <div className=" text-center">
-                <p className="bg-secondary p-2 mb-0">88%</p>
-                <span style={{ fontSize: "0.9rem" }}>Park win percentage</span>
+            <Col md={3}>
+              <div className={`text-center ${style.forPark}`}>
+                <p className="p-2 mb-0">88%</p>
+                <span style={{ fontSize: "0.9rem" }} className="text-secondary">
+                  Park win percentage
+                </span>
               </div>
             </Col>{" "}
-            <Col md={2}>
-              <div className=" text-center">
-                <p className="bg-secondary p-2 mb-0">88%</p>
-                <span style={{ fontSize: "0.9rem" }}>Total Matched Played</span>
+            <Col md={3}>
+              <div className={`text-center ${style.forTotal}`}>
+                <p className="p-2 mb-0">88%</p>
+                <span style={{ fontSize: "0.9rem" }} className="text-secondary">
+                  Total Matched Played
+                </span>
               </div>
             </Col>{" "}
           </Row>
           <Row className="pt-1 px-1">
             <Col>
-              <button className="px-4 py-2 bg-primary text-white border-0">
+              <button className="px-4 py-2 bg-primary user fw-bold text-white border-0">
                 Player Ranking
               </button>
             </Col>
           </Row>
+          <Row className="px-1">
+            <Col>
+              <p className="mb-0 text-secondary">Previous Upload</p>
+              <div className="d-flex gap-5">
+                <img
+                  src={player}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    border: "1px solid black",
+                  }}
+                />
+                <img
+                  src={player}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    border: "1px solid black",
+                  }}
+                />
+                <img
+                  src={player}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    border: "1px solid black",
+                  }}
+                />
+                <img
+                  src={player}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    border: "1px solid black",
+                  }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col
+          className="d-flex flex-column gap-3 pt-5"
+          md={{ offset: 1, span: 2 }}
+        >
+          <button className={`p-2 ${style.matchButton}`}>Setup a Match</button>
+          <button className="p-2">Previous Match</button>
+          <button className="p-2">Upload Record</button>
         </Col>
       </Row>
     </Container>
